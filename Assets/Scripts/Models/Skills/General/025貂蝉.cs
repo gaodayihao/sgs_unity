4fa100fa39aa2366b9678095fd99b16b16731f4b
wyx <3081879742@qq.com>
2022-08-07 00:42:43 +0800

    军争篇

--- Assets/Scripts/Models/Skills/General/025貂蝉.cs
+++ Assets/Scripts/Models/Skills/General/025貂蝉.cs
@@ -1,18 +1,44 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
 
-// public class 025貂蝉 : MonoBehaviour
-// {
-//     // Start is called before the first frame update
-//     void Start()
-//     {
-        
-//     }
+namespace Model
+{
+    public class 离间 : Active
+    {
+        public 离间(Player src) : base(src, "离间", false, 1) { }
 
-//     // Update is called once per frame
-//     void Update()
-//     {
-        
-//     }
-// }
+        public override int MaxCard()
+        {
+            return 1;
+        }
+
+        public override int MinCard()
+        {
+            return 1;
+        }
+
+        public override int MaxDest(List<Card> cards)
+        {
+            return 2;
+        }
+
+        public override int MinDest(List<Card> cards)
+        {
+            return 2;
+        }
+
+        public override bool IsValidDest(Player dest, List<Card> cards, Player firstDest = null)
+        {
+            return dest != Src && dest.general.gender;
+        }
+
+        public override async Task Execute(List<Player> dests, List<Card> cards, string additional)
+        {
+            await base.Execute(dests, cards, additional);
+
+            await Card.Convert<决斗>().UseCard(dests[1], new List<Player> { dests[0] });
+        }
+    }
+}
